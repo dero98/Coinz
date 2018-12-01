3b9c21607a5b793c5ab51c132cbd3b5d47404b87
derenikpogosayn <s1612425@sms.ed.ac.uk>
2018-12-01 02:04:28 +0000

    The colors of each marker is added. Also now we are saving rates in the SharedPrefrences file.

--- app/src/main/java/uk/ac/ed/inf/coinz/MainActivity.java
+++ app/src/main/java/uk/ac/ed/inf/coinz/MainActivity.java
@@ -1,7 +1,5 @@
 package uk.ac.ed.inf.coinz;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -10,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -18,8 +15,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawer;
     private final String tag = "MainActivity";
-    private String downloadDate = "2018/11/30"; // Format: YYYY/MM/DD
-    private final String preferencesFile = "MyPrefsFile"; // for storing preferences
     protected static MapFragment mapfrag= new MapFragment();
 
 
@@ -84,23 +79,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void onStart() {
         super.onStart();
-// Restore preferences
-        SharedPreferences settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
-// use ”” as the default value (this might be the first time the app is run)
-        downloadDate = settings.getString("lastDownloadDate", "");
-        Log.d(tag, "[onStart] Recalled lastDownloadDate is ’" + downloadDate + "’");
     }
     @Override
     public void onStop() {
         super.onStop();
-        Log.d(tag, "[onStop] Storing lastDownloadDate of " + downloadDate);
-// All objects are from android.context.Context
-        SharedPreferences settings = getSharedPreferences(preferencesFile,
-                Context.MODE_PRIVATE);
-// We need an Editor object to make preference changes.
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("lastDownloadDate", downloadDate);
-// Apply the edits!
-        editor.apply();
     }
 }
